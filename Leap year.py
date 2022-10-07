c93287ece4b6c896a3764257b4c236847c56e70a
Rahul-Dogney <49445986+RAHUL-DOGNEY@users.noreply.github.com>
2022-10-07 20:12:09 +0530

    I have committed to changes in Leap Year Code
    In this program i try to explain scenario happened  in Leap year.

--- Leap year.py
+++ Leap year.py
@@ -1,11 +1,33 @@
-i=int(input('enter a year'))
-if (i%4)==0:
-  if (i%100)==0:
-    if (i%400)==0:
-      print('leap year')
-    else:
-      print('not a lep year')
-  else:
-     print('not a lep year')
+# A year is considered a leap year if that year is exactly divisible by 4, except for years that end with 00(century year). 
+# A century year is a leap year if that is exactly divisible by 400. However, a year which not divisible by 400 and divisible by 100 is not a leap year.
+# Example:
+
+# 2004 is a leap year.
+# 2020 is a leap year.
+# 1900 is not a leap year.
+# 2013 is not a leap year.
+
+# Two ways of making program
+
+# Using Conditional Statement
+input_year = int(input("Enter the Year to be checked: "))
+if (( input_year%400 == 0)or (( input_year%4 == 0 ) and ( input_year%100 != 0))):
+    print("%d is Leap Year" %input_year)
 else:
-  print('not a lep year')
+    print("%d is Not the Leap Year" %input_year)
+    
+    
+    
+# Using Nested If
+input_year = int(input("Enter the Year to be checked: "))
+if(input_year%4 == 0):
+    if(input_year%100 == 0):
+        if(input_year%400 == 0):
+            print("%d is Leap Year" %input_year)
+        else:
+            print("%d is Not the Leap Year" %input_year)
+    else:
+        print("%d is Leap Year" %input_year)
+else:
+    print("%d is Not the Leap Year" %input_year)
+
